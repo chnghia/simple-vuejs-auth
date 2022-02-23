dae06f836b19cc851cd97a123a6a0d5cd32889c6
cuibapn2014 <56261497+cuibapn2014@users.noreply.github.com>
2022-02-23 23:59:12 +0700

    Load quote from graphql with fetch

--- hugo-project/static/assets/js/script.js
+++ hugo-project/static/assets/js/script.js
@@ -2,6 +2,8 @@ var open = document.getElementById("btn-open");
 var close = document.getElementById("btn-close");
 var mobile = document.getElementById("mobile-nav");
 
+document.head.innerHTML = document.head.innerHTML + "<base href='" + document.location.href + "' />";
+
 open.addEventListener("click",() => {
   mobile.classList.remove("hidden");
 });
